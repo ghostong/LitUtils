a4716fdc6996eebb0fff05fc0f6db9b4f72d3f28
李通 <ghostong@126.com>
2017-04-26 13:57:17 +0800

    添加空lidate,listring

---
+++ src/lidate.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace lit\litool;
+
+/**
+ * lidate: litool PHP 日期时间部分
+ * @author  litong
+ * @since   1.0
+ **/
+
+class lidate {
+
+    /** 
+     * init
+     * 测试调用
+     * @access public static
+     * @since  1.0 
+     * @return string
+     **/   
+    public static function init (){
+        return __CLASS__.':'.__FUNCTION__;
+    }
+
+}
---
+++ src/listring.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace lit\litool;
+
+/**
+ * listring: litool PHP 字符串部分
+ * @author  litong
+ * @since   1.0
+ **/
+
+class listring {
+    
+    /** 
+     * init
+     * 测试调用
+     * @access public static
+     * @since  1.0 
+     * @return string
+     **/   
+    public static function init (){
+        return __CLASS__.':'.__FUNCTION__;
+    }
+
+}
